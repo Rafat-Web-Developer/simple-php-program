19a2d68002453c508c4ffea9d041248ab71fc187
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-06 21:37:13 +0600

    arithmatic oparation

--- arithmetic-operation.php
+++ arithmetic-operation.php
@@ -8,6 +8,17 @@
         return $sum;
     }
     
+    // ------Find Subtraction--------
+    function subtraction($getNum1, $getNum2){
+        $sub = 0;
+        if($getNum1 > $getNum2){
+            $sub = $getNum1 - $getNum2;
+        }else{
+            $sub = $getNum2 - $getNum1;
+        }
+        return $sub;
+    }
+    
     // ------Find Multiplication--------
     function Multiplication($getNum1, $getNum2){
         $mul = $getNum1 * $getNum2;
@@ -19,8 +30,11 @@
 
     $number1 = 5;
     $number2 = 10;
+    echo "Number1 = ".$number1.', Number2 = '.$number2.'<br/>';
     $getResult = sum($number1, $number2);
     echo "Sum = ".$getResult.'<br/>';
+    $getResult = subtraction($number1, $number2);
+    echo "Subtraction = ".$getResult.'<br/>';
     $getResult = Multiplication($number1, $number2);
     echo "Multiplication = ".$getResult;
 ?>
