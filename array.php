0b6c278e8ee42693a1d74f0232b302e4b4bf7868
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-07 21:10:48 +0600

    Multidimensional array

--- array.php
+++ array.php
@@ -1,8 +1,20 @@
 <?php
 # //--simple Array--//
 $info = array('Rafat Hossain', 'rafat@gmail.com', '01876903613');
+echo "Simple Array :<br/>";
 for ($i = 0; $i < count($info); $i++) { 
-    echo 'Element ['.$i.'] = '.$info[$i].'<br/>';
+    switch ($i) {
+        case '0':
+            echo 'Name = ';
+            break;
+        case '1':
+            echo 'Email = ';
+            break;
+        default:
+            echo 'Phone = ';
+            break;
+    }
+    echo $info[$i].'<br/>';
 }
 
 # //--Associative Array--//
@@ -11,7 +23,33 @@ $information = array(
     'email' => 'rafat@gmail.com',
     'phone' => '01876903613'
 );
+echo "Associative Array :<br/>";
 echo 'Name = '.$information['name'].'<br/>';
 echo 'Email = '.$information['email'].'<br/>';
 echo 'Phone = '.$information['phone'].'<br/>';
+
+# //--Multidimensional Array--//
+$phones = array(
+    array('Phone1', 50, 5000),
+    array('Phone2', 50, 10000)
+);
+echo "Multidimensional Array :<br/>";
+for ($i = 0; $i < count($phones); $i++) { 
+    for ($j = 0; $j < count($phones[$i]); $j++) {
+        switch ($j) {
+            case '0':
+                echo 'Phone Name = ';
+                break;
+            case '1':
+                echo 'Stock = ';
+                break;
+            default:
+                echo 'Price = ';
+                break;
+        }
+        echo $phones[$i][$j].', ';
+    }
+    echo '<br/>';
+}
+
 ?>
