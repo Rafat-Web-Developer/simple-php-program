a4a79ad87a43e018789d2c40c76283eb122672c9
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-07 20:02:07 +0600

    Update array

--- array.php
+++ array.php
@@ -5,4 +5,13 @@ for ($i = 0; $i < count($info); $i++) {
     echo 'Element ['.$i.'] = '.$info[$i].'<br/>';
 }
 
+# //--Associative Array--//
+$information = array(
+    'name' => 'Rafat Hossain',
+    'email' => 'rafat@gmail.com',
+    'phone' => '01876903613'
+);
+echo 'Name = '.$information['name'].'<br/>';
+echo 'Email = '.$information['email'].'<br/>';
+echo 'Phone = '.$information['phone'].'<br/>';
 ?>
